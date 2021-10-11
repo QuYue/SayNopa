0f5c7e5beeeec57758dc8289ce5d3cd6eedda1e4
QuYue <quyue1541@gmail.com>
2021-10-11 21:38:07 +0800

    连接中 覆盖

--- SayNopa/client.py
+++ SayNopa/client.py
@@ -22,18 +22,18 @@ if __name__ == '__main__':
 本产品的版权归大连理工大学Goldminer实验室所有，
 欢迎使用不帕客户端。
     """)
-    print('连接中...')
+    print('\r连接中...', end='', flush=True)
     start_time = time.time()
     try:
         connect_result = requests.post(url_c, data=json.dumps({'send_time': start_time}))
     except:
         pass
     if connect_result.status_code != 200:
-        print(f'连接失败, Status Code: {connect_result.status_code}', flush=True)
+        print(f'\r连接失败, Status Code: {connect_result.status_code}', flush=True)
         print('退出客户端')
     else:
         end_time = time.time()
-        print(f"连接成功，延迟：{end_time-start_time :.2}s, 与服务端相差：{connect_result.json()['TimeDelay'] :.2}s\n")
+        print(f"\r连接成功，延迟：{end_time-start_time :.2}s, 与服务端时钟差：{connect_result.json()['TimeDelay'] :.2}s\n", flush=True)
 
         while True:
             input_content = input("输入图片路径，输入'exit'退出：")
